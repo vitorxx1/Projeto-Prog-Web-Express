cb06c0cdc7ec98c91ea97ebd154212e67d82694c
LuizFFerreira <93801358+LuizFFerreira@users.noreply.github.com>
2022-06-26 14:49:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 //parsers
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(upload.array()); 
+app.use(upload.array());
+app.use(cookieParser());
+app.use(session({secret: "Your secret key",
+                  resave: false,
+                  saveUninitialized: false,
+                  cookie: {
+                     expires: 600000,
+                  },
+})); 
 
 //engine para templating
 app.set('view engine', 'hbs');
@@ -23,11 +33,14 @@ var cadastro = require("./routes/cadastro");
 var login = require("./routes/login");
 var publicacoes = require("./routes/publicacao");
 var users = require("./routes/user");
+var logout = require("./routes/logout")
+
 
 app.use("/", raiz);
 app.use("/fipe", fipe);
 app.use("/cadastro", cadastro);
 app.use("/login", login);
+app.use("/logout", logout);
 app.use("/publicacoes", publicacoes);
 app.use("/users", users);
 
